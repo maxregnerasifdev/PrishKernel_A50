da7362751da4696008e66a8626ff677a8c92fada
neel0210 <neeljadhav0210@gmail.com>
2021-03-21 23:28:47 +0530

    attemp to fix : namie,c error

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -875,7 +875,7 @@ static void dx_release(struct dx_frame *frames)
 	if (frames[0].bh == NULL)
 		return;
 
-	info = &((struct dx_root *)frames[0].bh->b_data)->info
+	info = &((struct dx_root *)frames[0].bh->b_data)->info;
 	/* save local copy, "info" may be freed after brelse() */
 	indirect_levels = info->indirect_levels;
 	for (i = 0; i <= indirect_levels; i++) {
